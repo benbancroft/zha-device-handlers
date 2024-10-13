75ac667f21f1a66cf8aaea02dc737f564202c2bc
Ben Bancroft <ben@benbancroft.uk>
2024-10-13 20:28:11 +0100

    Add VOC quirk for IKEA VINDSTYRKA Air quality sensor

---
+++ zhaquirks/ikea/vindstyrka.py
@@ -0,0 +1,39 @@
+"""IKEA of Sweden VINDSTYRKA Air quality sensor."""
+
+from zigpy.quirks import CustomCluster
+from zigpy.quirks.v2 import QuirkBuilder
+from zigpy.quirks.v2.homeassistant.sensor import SensorDeviceClass
+import zigpy.types as t
+from zigpy.zcl.foundation import BaseAttributeDefs, ZCLAttributeAccess, ZCLAttributeDef
+
+
+class VOCIndex(CustomCluster):
+    """IKEA VOCIndex specific cluster."""
+
+    cluster_id = 0xFC7E
+
+    class AttributeDefs(BaseAttributeDefs):
+        """Attribute definitions."""
+
+        measured_value = ZCLAttributeDef(
+            id=0x0000,
+            type=t.Single,
+            mandatory=True,
+            access=(ZCLAttributeAccess.Read | ZCLAttributeAccess.Report),
+        )
+
+    @property
+    def _is_manuf_specific(self):
+        return False
+
+
+(
+    QuirkBuilder("IKEA of Sweden", "VINDSTYRKA")
+    .replaces(VOCIndex)
+    .sensor(
+        VOCIndex.AttributeDefs.measured_value.name,
+        VOCIndex.cluster_id,
+        device_class=SensorDeviceClass.VOLATILE_ORGANIC_COMPOUNDS,
+    )
+    .add_to_registry()
+)
